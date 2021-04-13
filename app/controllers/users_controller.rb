2446edd616df9b91a0a4ca57b9f69358a2ace5b4
Erez <erezfree29@gmail.com>
2021-04-13 12:14:00 -0700

    edit controller and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  def new
-    
+  def new 
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(username:params[:username],email:params[:email],passowrd:params[:passowrd])
   end
 end
